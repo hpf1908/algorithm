fbf3a783173ed8f57f4237bfd9b54b5e435d8ee4
hpf1908 <hpf1908@gmail.com>
2012-12-13 00:59:06 +0800

    add critical method for aov

--- graph/aovMatris.js
+++ graph/aovMatris.js
@@ -75,3 +75,91 @@ AovMatris.prototype.topoSort = function() {
     }
 }
 
+AovMatris.prototype.getEarlestTime = function(inputVertex , earlestMap) {
+
+    var ealestTime = - Number.MAX_VALUE;
+    var index = this.id2Index(inputVertex.id);
+
+    for (var i = 0; i < this.vertexs.length; i++) {
+        var vertex = this.vertexs[i];
+        var fMartis = this.matris[i];
+
+        if(fMartis && fMartis[index] != Number.MAX_VALUE) {
+            var tempVertex = this.vertexs[i];
+            var tempEe = earlestMap[tempVertex.id];
+            var ee = tempEe + fMartis[index];
+
+            if( ee  > ealestTime) {
+                ealestTime = ee;
+            }
+        }
+    }
+
+    return ealestTime < 0 ? 0 : ealestTime;
+}
+
+AovMatris.prototype.getLatestTime = function(inputVertex , earlestMap , latestMap) {
+
+    var latestTime = Number.MAX_VALUE;
+    var index = this.id2Index(inputVertex.id);
+    var fMartis = this.matris[index];
+
+    for (var i = 0; i < this.vertexs.length; i++) {
+        if(fMartis && fMartis[i] != Number.MAX_VALUE && this.vertexs[i]) {
+            var tempVertex = this.vertexs[i];
+            var tempLe = latestMap[tempVertex.id];
+            var le = tempLe - fMartis[i];
+
+            if(le < latestTime) {
+                latestTime = le;
+            }
+        }
+    }
+
+    return latestTime == Number.MAX_VALUE ? earlestMap[inputVertex.id] : latestTime;
+}
+
+AovMatris.prototype.criticalPath = function() {
+
+    var topoResult = this.topoSort();
+
+
+    if(!topoResult) {
+        return;
+    }
+
+    var eeMap = {} , leMap = {};
+
+    for (var i = 0; i < topoResult.length; i++) {
+        var vertex = topoResult[i];
+        var ee = this.getEarlestTime(vertex , eeMap);
+        eeMap[vertex.id] = ee;
+    }
+
+    for (var i = topoResult.length -1; i >= 0; i--) {
+        var vertex = topoResult[i];
+        var le = this.getLatestTime(vertex , eeMap , leMap);
+        leMap[vertex.id] = le;
+    }
+
+    var edgeEe = {} , edgeLe = {} , criticalPath = [];
+
+    for (var i = 0; i < this.edges.length; i++) {
+        var edge = this.edges[i];
+        edgeEe[i] = eeMap[edge.fromId];
+        edgeLe[i] = leMap[edge.toId] - edge.weight;
+
+        if(edgeEe[i] == edgeLe[i]) {
+            criticalPath.push(edge);
+        }
+    }
+
+    console.log(eeMap);
+    console.log(leMap);
+
+    console.log(edgeEe);
+    console.log(edgeLe);
+
+    return criticalPath;
+}
+
--- test/testAov.js
+++ test/testAov.js
@@ -25,17 +25,17 @@ inputVertexs.push(new Vertex(9 , '自动机理论'));
 
 var inputEdges = [];
 
-inputEdges.push(new Edge(1 , 2, 5));
-inputEdges.push(new Edge(1 , 4, 5));
+inputEdges.push(new Edge(1 , 2, 15));
+inputEdges.push(new Edge(1 , 4, 2));
 
-inputEdges.push(new Edge(2 , 5, 5));
+inputEdges.push(new Edge(2 , 5, 3));
 inputEdges.push(new Edge(2 , 6, 5));
 
-inputEdges.push(new Edge(3 , 2, 5));
-inputEdges.push(new Edge(3 , 6, 5));
+inputEdges.push(new Edge(3 , 2, 1));
+inputEdges.push(new Edge(3 , 6, 2));
 
-inputEdges.push(new Edge(4 , 7, 5));
-inputEdges.push(new Edge(4 , 8, 5));
+inputEdges.push(new Edge(4 , 7, 4));
+inputEdges.push(new Edge(4 , 8, 8));
 
 // inputEdges.push(new Edge(5 , 6, 5));
 
@@ -48,5 +48,45 @@ console.log('topoSort ---- test');
 var avoMatris = new AovMatris(inputVertexs,inputEdges);
 
 console.log(avoMatris.topoSort());
+
+/**
+ *
+ *  
+                                 10 后台编码完成  3
+    做网站  3 需求分析完成 5 设计完成                  联调完成 10 测试上线
+                                 7  前台编码完成  3
+ *
+ */
+
+var inputVertexs = [];
+
+inputVertexs.push(new Vertex(1 , '做网站'));
+inputVertexs.push(new Vertex(2 , '需求分析完成'));
+inputVertexs.push(new Vertex(3 , '设计完成'));
+inputVertexs.push(new Vertex(4 , '后台编码完成'));
+inputVertexs.push(new Vertex(5 , '前台编码完成'));
+inputVertexs.push(new Vertex(6 , '联调完成'));
+inputVertexs.push(new Vertex(7 , '测试上线'));
+
+var inputEdges = [];
+
+inputEdges.push(new Edge(1 , 2, 3));
+
+inputEdges.push(new Edge(2 , 3, 5));
+
+inputEdges.push(new Edge(3 , 4, 10));
+inputEdges.push(new Edge(3 , 5, 7));
+
+inputEdges.push(new Edge(4 , 6, 3));
+
+inputEdges.push(new Edge(5 , 6, 3));
+
+inputEdges.push(new Edge(6 , 7, 10));
+
+console.log('topoSort ---- criticalPath');
+
+var avoMatris = new AovMatris(inputVertexs,inputEdges);
+
+console.log(avoMatris.criticalPath());
 
 
